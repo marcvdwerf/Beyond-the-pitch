ae0df6aed20d7a5ff02552a803abbb67dbfaf918
marcvdwerf <marcvdwerf33@gmail.com>
2026-01-12 18:53:56 +0100

    Update partner-dashboard.js

--- dashboard/partner-dashboard.js
+++ dashboard/partner-dashboard.js
@@ -1,7 +1,7 @@
 /**
  * Beyond the Pitch - Partner Dashboard Logic (Geoptimaliseerd)
  */
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbzBfHwVlLA4B8t-UNBtMGQUxvwhww33SndRKIJRjn83WxtFPiHwU_ZiWsqZYDnytJCNrg/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbzDuYt-8z_lN_e63avbnrK8_Ik-67vt8t-zimn8VOvtz0glCgiEYOGC-Ywq_7ewZ1hrYA/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
     // 1. Check Autorisatie
