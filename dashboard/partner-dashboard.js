e43bb83a438ffa32da2f257f991d834955092a5e
marcvdwerf <marcvdwerf33@gmail.com>
2026-03-01 20:17:40 +0100

    Update partner-dashboard.js

--- dashboard/partner-dashboard.js
+++ dashboard/partner-dashboard.js
@@ -3,7 +3,7 @@
  * Versie: 4.0 - Veilige apiFetch + token validatie
  */
 
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwAZB3Rd9_xsj0xuoTucR3P0PlvkW4Nkolquia0HbTYvX_mqsi7TQ3XZW1WYmbaHQ_x/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxPkyCqIml8BmoV5btvqZ5l3rsB77P1gLvX7HFyE-_5UNaTx6v2GKfLcUzi4yZLxiGe2w/exec';
 
 let calendarInstance = null; // Globaal bijhouden zodat we hem kunnen destroyen bij re-sync
 
