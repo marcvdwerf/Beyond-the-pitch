a2301ed7d8ccf765e85ba3710cf3693b3c742d2d
marcvdwerf <marcvdwerf33@gmail.com>
2026-03-01 20:17:51 +0100

    Update admin-dashboard.js

--- dashboard/admin-dashboard.js
+++ dashboard/admin-dashboard.js
@@ -3,7 +3,7 @@
  * Versie: 4.0 - apiFetch + dynamische partner filter + echte revenue chart
  */
 
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwAZB3Rd9_xsj0xuoTucR3P0PlvkW4Nkolquia0HbTYvX_mqsi7TQ3XZW1WYmbaHQ_x/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxPkyCqIml8BmoV5btvqZ5l3rsB77P1gLvX7HFyE-_5UNaTx6v2GKfLcUzi4yZLxiGe2w/exec';
 
 let revenueChart = null;
 let allBookings  = [];
