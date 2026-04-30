2e3324f757aaed20660ea063b01f082ced690c8d
marcvdwerf <marcvdwerf33@gmail.com>
2026-04-30 11:50:40 +0200

    Update SHEET_API_URL to new endpoint

--- dashboard/admin-dashboard.js
+++ dashboard/admin-dashboard.js
@@ -3,7 +3,7 @@
  * Versie: 3.3
  */
 
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwo_jAnamlQ2h9HZ_0imBBURqILeJZn71WQy_svoGQZ7fFM9agCmSZS9t34AThXccN0gw/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxUJXzfIJliGl-DqTuyTI1LtGn2nCgyi7751HdBVLWCYdGNV1UXeytWQQcGT_L-BMbCGw/exec';
 
 let revenueChart = null;
 let allBookings = [];
