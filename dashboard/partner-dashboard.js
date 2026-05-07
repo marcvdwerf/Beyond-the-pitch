a492088ec8ee5158f88b4cb72177c567d721196f
marcvdwerf <marcvdwerf33@gmail.com>
2026-05-07 09:09:11 +0200

    Update partner-dashboard.js

--- dashboard/partner-dashboard.js
+++ dashboard/partner-dashboard.js
@@ -3,7 +3,7 @@
  * Versie: 3.2 - Mobile First, Anti-Undefined & Status Badges
  */
 
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwjFrusDC5ESzUoG0HENGan3xrzsWSLCAeSlDDerL8DLUL6GBjdu7SYS4aLV2cQOaZghA/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxbf7UMLSEZ1Xx4sg2mQv7zv3xCZXvG8j45khco_X_Rq-gv6bobgarBlSQTOqad4vR5sA/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
     const partnerID = sessionStorage.getItem("partnerID");
