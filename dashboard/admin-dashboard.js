a971d8a447b1e6ffac8214821d548a2a5a6b4565
marcvdwerf <marcvdwerf33@gmail.com>
2026-05-07 10:04:55 +0200

    Update admin-dashboard.js

--- dashboard/admin-dashboard.js
+++ dashboard/admin-dashboard.js
@@ -3,7 +3,7 @@
  * Versie: 3.3
  */
 
-const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbxbf7UMLSEZ1Xx4sg2mQv7zv3xCZXvG8j45khco_X_Rq-gv6bobgarBlSQTOqad4vR5sA/exec';
+const SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbztfXQQ3ggAZKY710BxvXfwVMs5jja7ymo6zLfacNXh6-gbNyjHRo31xC0o9yNgdV0bdw/exec';
 
 let revenueChart = null;
 let allBookings = [];
